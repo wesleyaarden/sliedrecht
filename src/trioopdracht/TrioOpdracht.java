8ebcd38515a98e4b003250d6a3ae734e14b1a1f6
Wesley Aarden <wjmaarden@avans.nl>
2016-11-25 09:35:38 +0100

    stukje verbeterd

--- src/trioopdracht/TrioOpdracht.java
+++ src/trioopdracht/TrioOpdracht.java
@@ -1,21 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package trioopdracht;
 
-/**
- *
- * @author Wesley Aarden
- */
-public class TrioOpdracht {
+import javax.swing.JFrame;
 
-    /**
-     * @param args the command line arguments
-     */
+
+
+
+public class TrioOpdracht extends JFrame {
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        JFrame frame = new TrioOpdracht(); {
+            
+         }
     }
+        }
+            
+     
+
     
-}
+    
+
